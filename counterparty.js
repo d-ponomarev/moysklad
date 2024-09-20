9d2c3a4909d57ad72676bbb21d2cdbb8da407758
d-ponomarev <dponomarev2611@gmail.com>
2024-09-20 18:01:13 +0300

    hgfhf

--- counterparty.js
+++ counterparty.js
@@ -138,6 +138,10 @@ app.post("/retaildemand/create", (req, res) => {
   res.status(200).json({ meta: meta });
 });
 
+app.get("/test", (req, res) => {
+  res.status(200).json({ message: "success" });
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
