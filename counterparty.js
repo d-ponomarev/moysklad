43696246cd904f33b98d9ddb65fd39f8133a2284
d-ponomarev <dponomarev2611@gmail.com>
2024-09-20 17:47:38 +0300

    fdsfsd

--- counterparty.js
+++ counterparty.js
@@ -1,135 +1,144 @@
+const express = require("express");
 const axios = require("axios");
+const app = express();
 
 const TOKEN = "e8d72cbb586832eb3715b04ce61e17cda8d65048";
 
-module.exports = async (req, res) => {
-  const { method, url } = req;
+app.use(express.json());
 
+const verifyAuthToken = (req, res, next) => {
   const authToken = req.headers["lognex-discount-api-auth-token"];
   if (authToken !== "123") {
     return res.status(401).json({ error: "Неверный токен авторизации" });
   }
-
-  if (url === "/counterparty" && method === "GET") {
-    const { search, retailStoreId } = req.query;
-
-    if (!search) {
-      return res.status(400).json({ error: "Отсутствует параметр search" });
-    }
-
-    try {
-      const counterpartyReponse = await axios.get(
-        `https://api.moysklad.ru/api/remap/1.2/entity/counterparty/${search}`,
-        {
-          headers: {
-            Authorization: `Bearer ${TOKEN}`,
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      const counterparty = counterpartyReponse.data;
-
-      if (counterparty.id) {
-        res.status(200).json({
-          rows: [
-            {
-              id: counterparty.id,
-              name: counterparty.name,
-              discountCardNumber: counterparty.discountCardNumber,
-            },
-          ],
-        });
-      } else {
-        res.status(404).json({ error: "Контрагент не найден" });
-      }
-    } catch (error) {
-      console.error("Ошибка при запросе к API МойСклад:", error);
-      res.status(500).json({ error: "Ошибка при запросе к API МойСклад" });
-    }
-  } else if (url === "/counterparty/detail" && method === "POST") {
-    const { meta } = req.body;
-
-    try {
-      const counterpartyReponse = await axios.get(
-        `https://api.moysklad.ru/api/remap/1.2/entity/counterparty/${meta.id}`,
-        {
-          headers: {
-            Authorization: `Bearer ${TOKEN}`,
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      const counterparty = counterpartyReponse.data;
-
-      if (counterparty.id) {
-        let bonusField = null;
-        if (
-            counterparty.attributes &&
-            counterparty.attributes.length > 0
-        ) {
-          bonusField = counterparty.attributes.find(
-            (attr) => attr.name === "Бонусы"
-          );
-        }
-
-        res.status(200).json({
-          bonusProgram: {
-            agentBonusBalance: bonusField.value,
-          },
-        });
-      } else {
-        res.status(404).json({ error: "Контрагент не найден" });
-      }
-    } catch (error) {
-      console.error("Ошибка при запросе к API МойСклад:", error);
-      res.status(500).json({ error: "Ошибка при запросе к API МойСклад"});
-    }
-  } else if (url === "/retaildemand/recalc" && method === "POST") {
-    const { meta } = req.body;
-    
-    try {
-        const counterpartyReponse = await axios.get(
-          `https://api.moysklad.ru/api/remap/1.2/entity/counterparty/${meta.agent.meta.id}`,
-          {
-            headers: {
-              Authorization: `Bearer ${TOKEN}`,
-              "Content-Type": "application/json",
-            },
-          }
-        );
-  
-        const counterparty = counterpartyReponse.data;
-  
-        if (counterparty.id) {
-          let tags = counterparty.tags;
-          if (
-              counterparty.attributes &&
-              counterparty.attributes.length > 0
-          ) {
-            bonusField = counterparty.attributes.find(
-              (attr) => attr.name === "Бонусы"
-            );
-          }
-  
-          res.status(200).json({
-            bonusProgram: {
-              agentBonusBalance: bonusField.value,
-            },
-          });
-        } else {
-          res.status(404).json({ error: "Контрагент не найден" });
-        }
-      } catch (error) {
-        console.error("Ошибка при запросе к API МойСклад:", error);
-        res.status(500).json({ error: "Ошибка при запросе к API МойСклад"});
-      }
-  } else if (url === "/retaildemand/create" && method === "POST") {
-    const { meta } = req.body;
-    console.log(meta);
-    res.status(200).json({meta: meta});
-  } else {
-    res.status(404).json({ error: "Маршрут не найден" });
-  }
+  next();
 };
+
+app.use(verifyAuthToken);
+
+app.get("/counterparty", async (req, res) => {
+  const { search } = req.query;
+
+  if (!search) {
+    return res.status(400).json({ error: "Отсутствует параметр search" });
+  }
+
+  try {
+    const response = await axios.get(
+      `https://api.moysklad.ru/api/remap/1.2/entity/counterparty/${search}`,
+      {
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const counterparty = response.data;
+
+    if (counterparty.id) {
+      res.status(200).json({
+        rows: [
+          {
+            id: counterparty.id,
+            name: counterparty.name,
+            discountCardNumber: counterparty.discountCardNumber,
+          },
+        ],
+      });
+    } else {
+      res.status(404).json({ error: "Контрагент не найден" });
+    }
+  } catch (error) {
+    console.error("Ошибка при запросе к API МойСклад:", error);
+    res.status(500).json({ error: "Ошибка при запросе к API МойСклад" });
+  }
+});
+
+app.post("/counterparty/detail", async (req, res) => {
+  const { meta } = req.body;
+
+  try {
+    const response = await axios.get(
+      `https://api.moysklad.ru/api/remap/1.2/entity/counterparty/${meta.id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const counterparty = response.data;
+
+    if (counterparty.id) {
+      let bonusField = null;
+      if (counterparty.attributes && counterparty.attributes.length > 0) {
+        bonusField = counterparty.attributes.find(
+          (attr) => attr.name === "Бонусы"
+        );
+      }
+
+      res.status(200).json({
+        bonusProgram: {
+          agentBonusBalance: bonusField ? bonusField.value : 0,
+        },
+      });
+    } else {
+      res.status(404).json({ error: "Контрагент не найден" });
+    }
+  } catch (error) {
+    console.error("Ошибка при запросе к API МойСклад:", error);
+    res.status(500).json({ error: "Ошибка при запросе к API МойСклад" });
+  }
+});
+
+app.post("/retaildemand/recalc", async (req, res) => {
+  const { meta } = req.body;
+
+  try {
+    const response = await axios.get(
+      `https://api.moysklad.ru/api/remap/1.2/entity/counterparty/${meta.agent.meta.id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const counterparty = response.data;
+
+    if (counterparty.id) {
+      let bonusField = null;
+      if (counterparty.attributes && counterparty.attributes.length > 0) {
+        bonusField = counterparty.attributes.find(
+          (attr) => attr.name === "Бонусы"
+        );
+      }
+
+      res.status(200).json({
+        bonusProgram: {
+          agentBonusBalance: bonusField ? bonusField.value : 0,
+        },
+      });
+    } else {
+      res.status(404).json({ error: "Контрагент не найден" });
+    }
+  } catch (error) {
+    console.error("Ошибка при запросе к API МойСклад:", error);
+    res.status(500).json({ error: "Ошибка при запросе к API МойСклад" });
+  }
+});
+
+app.post("/retaildemand/create", (req, res) => {
+  const { meta } = req.body;
+  console.log("meta:", meta);
+
+  res.status(200).json({ meta: meta });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
