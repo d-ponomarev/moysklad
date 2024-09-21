7ed0ffd6bb38ff2fe70f717f34ec597941507935
d-ponomarev <dponomarev2611@gmail.com>
2024-09-21 14:35:39 +0300

    gfdgdf

--- api/index.js
+++ api/index.js
@@ -347,9 +347,9 @@ app.post("/retaildemand/recalc", (req, res) => {
           },
         },
         quantity: 1,
-        price: 700,
+        price: 1000,
         discountPercent: 0,
-        discountedPrice: 0,
+        discountedPrice: 1000,
       },
     ],
     bonusProgram: {
